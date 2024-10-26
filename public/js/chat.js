fc1cfc8fa8d155a0ab91ed50385b690b00821a8d
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-26 15:28:06 +0530

    implement events emit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io()
+const socket =  io() //send event and recv event
+
+socket.on('countUpdated',(count)=>{
+    console.log("The count has been update!", count)
+
+})
+
+document.querySelector('#increment').addEventListener('click',()=>{
+    console.log('Clicked')
+    socket.emit('increment');
+});
+
+
