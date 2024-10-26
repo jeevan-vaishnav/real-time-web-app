e1669fbedb46a2c2ecd6edd0254a75a6b9b0e71b
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-26 13:48:07 +0530

    implemented socket.io

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-const path = require('path')
-const express = require('express');
+const path = require("node:path");
+const http = require("node:http");
+const express = require("express");
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
 
 const port = process.env.PORT || 3000;
 
-const publicDirectoryPath = path.join(__dirname,'../public');
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-
-app.listen(port,()=>{
-    console.log(`Server is up on port ${port}`)
+io.on('connection',()=>{
+    console.log('New websocket IO')
 })
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
