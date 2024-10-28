3e8a17d05d5fad00c24b64b35033e6965e36200b
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-28 13:42:50 +0530

    Implemented location link
    ticket #15

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,41 @@
 const socket = io(); //send event and recv event
 
 //elements
-const $messageForm = document.querySelector('#message-form');
-const $messageFormInput = $messageForm.querySelector('input');
-const $messageFormButton = $messageForm.querySelector('button');
-const $locationButton = document.querySelector('#send-location'); 
-const $messages = document.querySelector('#messages');
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
 
 //Templates
-const messageTemplate = document.querySelector('#message-template').innerHTML; 
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector('#locationMessage-template').innerHTML
+
+
 
 socket.on("message", (msg) => {
   console.log(msg);
-  const html = Mustache.render(messageTemplate,{message:msg});
-  $messages.insertAdjacentHTML('beforeend',html);
+  const html = Mustache.render(messageTemplate, { message: msg });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
+
+socket.on('locationMessage',(url)=>{
+    console.log(url);
+    const html = Mustache.render(locationMessageTemplate,{url:url});
+    $messages.insertAdjacentHTML('beforeend',html);
+})
 
 $messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   //disable
-  $messageFormButton.setAttribute('disabled','disabled');
+  $messageFormButton.setAttribute("disabled", "disabled");
 
-  const message = e.target.elements.messageInput.value
+  const message = e.target.elements.messageInput.value;
 
   socket.emit("sendMessage", message, (error) => {
-    $messageFormButton.removeAttribute('disabled');
-    $messageFormInput.value = ''
-    $messageFormInput.focus()
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
     if (error) {
       return console.log(error);
     }
@@ -35,21 +44,26 @@ $messageForm.addEventListener("submit", (e) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
 
-  $locationButton.setAttribute('disabled',"disabled")
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  $locationButton.setAttribute("disabled", "disabled");
 
   if (!navigator.geolocation) {
     return alert("Geolocation not supported by your browser");
   }
 
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    }, () => {
-        $locationButton.removeAttribute('disabled');
-        console.log('Location shared!')
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $locationButton.removeAttribute("disabled");
+        console.log("Location shared!");
+      }
+    );
   });
 });
